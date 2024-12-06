f511ec71c0e534cb3665515fceed05a891ddfd23
kevin-079 <5026231079@student.its.ac.id>
2024-12-06 18:24:54 +0700

    Update CellStatus.java

--- CellStatus.java
+++ CellStatus.java
@@ -1,3 +1,13 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231008 - Batara Haryo Yudanto
+ * 2 - 5026231079 - Kevin Nathanael
+ * 3 - 5026231089 - SYusuf Acala Sadurjaya Sri Krisna
+ */
+
 public enum CellStatus {
     GIVEN,         // clue, no need to guess
     TO_GUESS,      // need to guess - not attempted yet
