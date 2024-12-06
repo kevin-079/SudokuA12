305078fbab0e744140928d1ec98e36c937cf4ac3
kevin-079 <5026231079@student.its.ac.id>
2024-12-06 18:26:02 +0700

    Update SudokuConstants.java

--- SudokuConstants.java
+++ SudokuConstants.java
@@ -1,6 +1,13 @@
 /**
- * Define the named constants used in many classes.
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231008 - Batara Haryo Yudanto
+ * 2 - 5026231079 - Kevin Nathanael
+ * 3 - 5026231089 - SYusuf Acala Sadurjaya Sri Krisna
  */
+
 public class SudokuConstants {
     /** Size of the board */
     public static final int GRID_SIZE = 9;
